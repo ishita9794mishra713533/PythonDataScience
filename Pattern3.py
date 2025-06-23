1bd54f200968346c3af0c26767f7ab7e9414cba3
Ishita Mishra <ishika2107mishra@gmail.com>
2025-06-23 07:23:50 -0700

    Delete Pattern3.py

--- Pattern3.py
+++
@@ -1,9 +0,0 @@
-import turtle
-Colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
-t = turtle.Pen()
-turtle.bgcolor('black')
-for x in range(360):
-    t.pencolor(Colors[x%6])
-    t.width(x//100+1)
-    t.forward(x)
-    t.left(59)
