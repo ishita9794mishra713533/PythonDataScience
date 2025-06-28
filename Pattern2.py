afb9e6240f74852bb19376d858b9006db0b88e72
Ishita Mishra <ishika2107mishra@gmail.com>
2025-06-28 10:45:46 -0700

    Python Diaries

---
+++ Pattern2.py
@@ -0,0 +1,19 @@
+from turtle import *
+# heart
+
+pencolor("red")
+fillcolor("red")
+speed("slowest")
+begin_fill()
+
+left(140)
+forward(180)
+circle(-90,200)
+setheading(60)
+circle(-90,200)
+forward(180)
+end_fill()
+hideturtle()
+
+mainloop()
+
